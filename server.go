a20cd2f3f290e6097f4ab17c47da33279cf12fd6
Xiaoguang Wang <wxiaoguang@gmail.com>
2020-02-06 12:03:17 +0800

    small optimiztion to routeFunc

--- server.go
+++ server.go
@@ -378,16 +378,18 @@ func routeFunc(dn string, funcNames []string) string {
 	bestPick := ""
 	bestPickWeight := 0
 	dnMatch := "," + strings.ToLower(dn)
+	var weight int
 	for _, fn := range funcNames {
 		if strings.HasSuffix(dnMatch, "," + fn) {
 			//  empty string as 0, no-comma string 1 , etc
-			c := strings.Count(fn, ",") + 1
 			if fn == "" {
-				c = 0
+				weight = 0
+			} else {
+				weight = strings.Count(fn, ",") + 1
 			}
-			if c > bestPickWeight {
+			if weight > bestPickWeight {
 				bestPick = fn
-				bestPickWeight = c
+				bestPickWeight = weight
 			}
 		}
 	}
